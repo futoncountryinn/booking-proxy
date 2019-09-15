b6e0d63c23641777395362f0aaa1e421708d5993
Keenan Thomson <keenanthomson@gmail.com>
2019-09-15 07:37:06 -0400

    Make performance enhancements to service

--- proxy/server.js
+++ proxy/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const axios = require('axios');
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -13,3 +14,12 @@ app.listen(port, () => {
   console.log(`server running at: ${port}`);
 });
 
+app.get('/reservation', (req, res) => {
+  const url = `http://localhost:3002/reservation`;
+  axios.get(url).then((response) => {res.send(response.data)});
+});
+
+app.post('/reservation', (req, res) => {
+  const url = `http://localhost:3002/reservation`;
+  axios.post(url).then((response) => {res.send(response.data)});
+});
